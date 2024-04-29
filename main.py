95e58d1207eb645f34614c7488fe599fc228e02e
Pasindu <pasindusandakan@gmail.com>
2024-04-29 17:57:17 +0530

    data ingection pipeline added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingection import DataIngestionTrainingPipeline
 
-logger.info("wine quality!")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
